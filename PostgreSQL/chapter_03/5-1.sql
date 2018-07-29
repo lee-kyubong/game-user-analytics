1258589d152a14ef1acc3652a52db9b929306a97
lee-kyubong <kyubonglee.sd@gmail.com>
2018-07-29 11:07:42 +0900

    Create tables

---
+++ PostgreSQL/chapter_03/5-1.sql
@@ -0,0 +1,16 @@
+-- 규봉: 경로지정 후 \i를 통해 test.sql initializing
+
+
+DROP TABLE IF EXISTS mst_users;
+CREATE TABLE mst_users(
+    user_id         varchar(255)
+  , register_date   varchar(255)
+  , register_device integer
+);
+
+INSERT INTO mst_users
+VALUES
+    ('U001', '2016-08-26', 1)
+  , ('U002', '2016-08-26', 2)
+  , ('U003', '2016-08-27', 3)
+;
