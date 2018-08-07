07098dd53acefdca78b0fba5a62b1276b919ea3e
lee-kyubong <kyubonglee.sd@gmail.com>
2018-08-07 11:14:36 +0900

    Update codes

--- PostgreSQL/chapter_03/08/8-4.sql
+++ PostgreSQL/chapter_03/08/8-4.sql
@@ -18,3 +18,45 @@ VALUES
   , ('book', 'B003', 10000)
   , ('book', 'B004',  5000)
 ;
+
+WITH
+  product_sale_ranking AS (
+    SELECT
+      category_name
+      , product_id
+      , sales
+      , ROW_NUMBER() OVER(PARTITION BY category_name ORDER BY sales DESC) AS rank
+    FROM
+      product_sales
+  )
+  SELECT *
+  FROM product_sale_ranking
+  ;
+
+  -- 유사테이블 생성
+  WITH
+  mst_devices AS (
+              SELECT 1 AS device_id, 'PC' AS device_name
+    UNION ALL SELECT 2 AS device_id, 'PHONE' AS device_name
+    UNION ALL SELECT 3 AS device_id, 'app' AS device_name
+  )
+  SELECT *
+  FROM mst_devices
+  ;
+
+
+  WITH
+  mst_devices AS (
+              SELECT 1 AS device_id, 'PC' AS device_name
+    UNION ALL SELECT 2 AS device_id, 'PHONE' AS device_name
+    UNION ALL SELECT 3 AS device_id, 'app' AS device_name
+  )
+  SELECT
+    u.user_id
+    , d.device_name
+  FROM
+    mst_users AS u
+    LEFT JOIN
+      mst_devices AS d
+      ON u.register_device = d.device_id
+  ;
