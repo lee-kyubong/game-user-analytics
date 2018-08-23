68cd5e88534eb9d75a1bf45e7efcefb34b647ceb
lee-kyubong <kyubonglee.sd@gmail.com>
2018-08-23 11:59:01 +0900

    Daily exercises

--- PostgreSQL/lab/WITH.sql
+++ PostgreSQL/lab/WITH.sql
@@ -55,3 +55,28 @@ g_dt AS (
   ORDER BY
     ranking
 ;
+
+
+-- 고객의 가입일 당시 나이를 산출하고 나이 순위를 매겨보기
+WITH
+age AS (
+  select
+    user_id
+    , (cast(substr(register_date, 1, 4) as int) - cast(substr(birth_date, 1, 4) as int)) as age
+  from
+    mst_users
+)
+ SELECT
+  u.user_id
+  , u.sex
+  , u.register_device
+  , a.age
+  , ROW_NUMBER() OVER(ORDER BY age DESC) as r
+FROM
+  mst_users as u
+  INNER JOIN
+  age as a
+  ON u.user_id = a.user_id
+ORDER BY
+  r ASC -- select 문서 생성한 컬럼 기준으로 정렬 가능하다.
+;
