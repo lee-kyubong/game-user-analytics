d1cc6a358d6202058d9e1e8307d08b7e0d989d9c
lee-kyubong <kyubonglee.sd@gmail.com>
2018-08-24 23:45:38 +0900

    Update codes

--- PostgreSQL/chapter_05/11/11-1.sql
+++ PostgreSQL/chapter_05/11/11-1.sql
@@ -288,3 +288,29 @@ GROUP BY
 ORDER BY
   action_day_count
 ;
+
+
+-- Decile
+WITH
+user_purchase_amount AS (
+  SELECT
+    user_id
+    , SUM(amount) AS purchase_amount
+  FROM
+    action_log
+  WHERE
+    action = 'purchase'
+  GROUP BY
+    user_id
+)
+, users_with_decile AS (
+  SELECT
+    user_id
+    , purchase_amount
+    , ntile(10) OVER(ORDER BY purchase_amount DESC) AS decile
+  FROM
+    user_purchase_amount
+)
+SELECT *
+FROM users_with_decile
+;
