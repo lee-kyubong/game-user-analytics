be6ca0820329881e11ae7bcead259f20ad3a4389
lee-kyubong <kyubonglee.sd@gmail.com>
2018-08-26 23:26:13 +0900

    Update codes

--- PostgreSQL/chapter_05/11/11-1.sql
+++ PostgreSQL/chapter_05/11/11-1.sql
@@ -314,3 +314,40 @@ user_purchase_amount AS (
 SELECT *
 FROM users_with_decile
 ;
+
+
+-- decile별로 그룹핑
+WITH
+user_purchase_amount AS (
+  SELECT
+    user_id
+    , SUM(amount) AS purchase_amount
+  FROM
+    action_log
+  WHERE
+    action = 'purchase'
+  GROUP BY
+    user_id
+)
+, users_with_decile AS (
+  SELECT
+    user_id
+    , purchase_amount
+    , ntile(10) OVER(ORDER BY purchase_amount DESC) AS decile
+  FROM
+    user_purchase_amount
+)
+, decile_with_purchase_amount AS (
+  SELECT
+    decile
+    , SUM(purchase_amount) AS sum_amount
+    , AVG(purchase_amount) AS avg_amount
+    , SUM(SUM(purchase_amount)) OVER(ORDER BY decile) AS cum_amount
+    , SUM(SUM(purchase_amount)) OVER () AS total_amount
+  FROM
+    users_with_decile
+    GROUP BY
+      decile
+)
+SELECT *
+  FROM decile_with_purchase_amount;
