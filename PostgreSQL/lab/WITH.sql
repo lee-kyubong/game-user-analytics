4adacec756e1b371d9104e41650476e09c1058fc
Kyubong Theodore Lee <kyubonglee.sd@gmail.com>
2018-10-14 22:53:15 +0900

    Update WITH.sql

--- PostgreSQL/lab/WITH.sql
+++ PostgreSQL/lab/WITH.sql
@@ -93,3 +93,58 @@ from
     action
   order by user_id
 ;
+
+
+----------
+select (select count(*) from sample51 where no = 1) as a, count(*) from sample51;
+
+
+
+
+-- 이전 6일 기록과 함께 이동평균 구하기
+
+
+SELECT
+ dt
+ , SUM(purchase_amount) AS total_amount
+ , AVG(SUM(purchase_amount))
+   OVER(ORDER BY dt ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+   AS seven_day_avg
+ , CASE
+   WHEN
+     COUNT(*) OVER(ORDER BY dt ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) = 7
+   THEN
+     AVG(SUM(purchase_amount))
+     OVER(ORDER BY dt ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+ END
+ AS seven_day_avg_strict
+FROM purchase_log
+GROUP BY dt
+ORDER BY dt
+;
+
+
+-- 앞서 생성한 WITH 구문 활용법
+WITH
+daily_purchase AS (
+ SELECT
+   dt
+   -- 연, 월, 일 각각 추출
+   , substr(dt, 1, 4) AS year
+   , substr(dt, 6, 2) AS month
+   , substr(dt, 9, 2) AS date
+   , SUM(purchase_amount) AS purchase_amount
+ FROM purchase_log
+ GROUP BY dt
+)
+SELECT
+ dt
+ , CONCAT(year, '-', month) AS year_month -- WITH 구문에서 만든 컬럼 사용
+ , purchase_amount -- WITH 구문에서 만든 컬럼 사용
+ , SUM(purchase_amount)
+   OVER(PARTITION BY month ORDER BY dt ROWS UNBOUNDED PRECEDING)
+ AS agg_amount
+FROM daily_purchase
+ORDER BY dt
+;
+
